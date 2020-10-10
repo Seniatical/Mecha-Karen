1aa59e5065c2add7bf87be3d34dbfe695025c4f3
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-10-10 20:27:13 +0100

    Update bot.py

--- bot.py
+++ bot.py
@@ -67,64 +67,11 @@ async def is_ready_bot():
 
 Time.start()
 
-@bot.event
-async def on_command_error(ctx, error):
-    embed = discord.Embed(
-        title='',
-        color=discord.Color.red()
-    )
-    if isinstance(error, commands.BadArgument):
-        embed.add_field(name='Finish the command', value='Finish the command off! smh.')
-        await ctx.send(embed=embed)
-    if isinstance(error, commands.MissingPermissions):
-        embed.add_field(name='Invalid Permissions', value=f'You dont have {error.missing_perms} permissions')
-        await ctx.send(embed=embed)
-    if isinstance(error, commands.CommandNotFound):
-        pass
-    if isinstance(error, commands.CommandOnCooldown):
-        if error.retry_after < 60:
-            f=round(error.retry_after) / 60
-            embed.add_field(name=f'Your on cooldown!', value=f'Stop trying. Wait {round(error.retry_after, 0)} seconds and retry again.')
-            await ctx.send(embed=embed)
-    if isinstance(error, commands.ArgumentParsingError):
-        embed.add_field(name='Error', value='Something Went wrong. Reloading Mecha Karen!')
-        await ctx.send(embed=embed)
-        await bot.sleep(10)
-    if isinstance(error, commands.BadUnionArgument):
-        embed.add_field(name='Error', value='Something Went Wrong. Reloading Mecha Karen!')
-        await ctx.send(embed=embed)
-    #if isinstance(error, commands.)
-
-@bot.event
-async def on_member_join(member):
-    print(f'{member} has joined')
-
-@bot.event
-async def on_member_remove(member):
-    print(f'{member} has left')
-
-@bot.event
-async def on_guild_join(guild):
-    with open('economy.json', 'r') as f:
-        guilds = json.load(f)
-
-    guilds[str(guild.id)] = '-'
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-@bot.event
-async def on_guild_remove(guild):
-    with open('prefixes.json', 'r') as f:
-        prefixes = json.load(f)
-
-    prefixes.pop(str(guild.id))
-
-    with open('prefixes.json', 'w') as f:
-        json.dump(prefixes, f, indent=4)
-
-# End of Events
 # Cog Loads
+
+for filename in os.listdir('./cogs'):
+    if filename.endswith('.py'):
+        bot.load_extension(f'cogs.{filename[:-3]}')
 
 @bot.command()
 @commands.cooldown(1, 60, BucketType.user)
@@ -139,15 +86,5 @@ async def load(ctx, extention):
 async def disable(ctx, extention):
     bot.unload_extension(f'cogs.{extention}')
     await ctx.send(f'{extention} cog has been disabled')
-
-for filename in os.listdir('./cogs'):
-    if filename.endswith('.py'):
-        bot.load_extension(f'cogs.{filename[:-3]}')
-
-@bot.command(aliases=['Action'])
-async def Actions(ctx):
-        guild = ctx.author.guild
-        entries = await guild.audit_logs(limit=None, user=guild.me).flatten()
-        await ctx.send('The server has made {} moderation actions.'.format(len(entries)))
 
 bot.run(Token)
