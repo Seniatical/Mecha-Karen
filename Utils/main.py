fe96a87e9050e3f9f5f9cb252bc78fda5508cc50
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-10-10 21:04:31 +0100

    Add files via upload

---
+++ Utils/main.py
@@ -0,0 +1,71 @@
+from typing import Dict, Any
+
+import discord
+from discord.ext import commands
+import json
+import os
+from Utils import Token
+from Utils.Secondary import *
+import requests
+
+def get_prefix(bot, message):
+    with open('JSON/prefixes.json', 'r') as f:
+        prefixes = json.load(f)
+    return prefixes[str(message.guild.id)]
+
+def COVID():
+    r = requests.get('https://covid19-api.org/api/status')
+    x = r.json()
+
+    COUNTRY = len(x)
+
+    COVID.cases = 0
+    COVID.deaths = 0
+    COVID.recovered = 0
+
+    for i in range(COUNTRY):
+        count = x[i]['cases']
+        COVID.cases += count
+
+    for i in range(COUNTRY):
+        count = x[i]['deaths']
+        COVID.deaths += count
+
+    for i in range(COUNTRY):
+        count = x[i]['recovered']
+        COVID.recovered += count
+
+def COVID_SPECIFIC(country):
+    country = country.upper()
+    r = requests.get('https://covid19-api.org/api/status/{}'.format(country))
+    x = r.json()
+    try:
+        COVID_SPECIFIC.country = x['country']
+        COVID_SPECIFIC.last_update = x['last_update']
+        COVID_SPECIFIC.cases = x['cases']
+        COVID_SPECIFIC.deaths = x['deaths']
+        COVID_SPECIFIC.recovered = x['recovered']
+        COVID_SPECIFIC.error = 'nil'
+    except KeyError:
+        COVID_SPECIFIC.error = 'Country Not found. Follow the format of `GB` or `gb`.\n**Use `-Covid Countries`** to view all countries!\nDMs need to be open.'
+
+try:
+    TOKEN = Token.token
+except Exception:
+    print('Your token is invalid. Regen a new one.')
+
+try:
+    BOT_PREF = commands.Bot(command_prefix='-', case_insensitive=True)
+except NotImplementedError:
+    pass
+
+INF = 10000000000000000000000000000000000000000000000000000000000
+
+channels = {
+    'tracking':'👀tracking',
+    'logs':'logs',
+    'star':'⭐starboard'
+}
+
+
+
