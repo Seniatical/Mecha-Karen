49a54037b1dfa70b3a5de2073a46f1b21280a12f
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-10-26 10:31:55 +0000

    The database manager for SQLite3.
    Utils/db.py
    Change the paths
    Thank Carberra for the orginal file

---
+++ Utils/db.py
@@ -0,0 +1,63 @@
+from os.path import isfile
+from sqlite3 import connect
+import os
+#from apscheduler.triggers.cron import CronTrigger
+
+'''
+os.chdir('C:\\Users\\isa1b.DESKTOP-GMQ5DPV.000.001\\PycharmProjects\\Discord Bot\\Utils\\SQL\\Data')
+
+DB_PATH = 'DB\\database.db'
+BUILD_PATH = "build.sql"
+
+cxn = connect(DB_PATH, check_same_thread=False)
+cur = cxn.cursor()
+
+def with_commit(func):
+	def inner(*args, **kwargs):
+		func(*args, **kwargs)
+		commit()
+	return inner
+
+@with_commit
+def build():
+	if isfile(BUILD_PATH):
+		scriptexec(BUILD_PATH)
+
+def commit():
+	cxn.commit()
+
+def autosave(sched):
+	sched.add_job(commit, CronTrigger(second=0))
+
+def close():
+	cxn.close()
+
+def field(command, *values):
+	cur.execute(command, tuple(values))
+	if (fetch := cur.fetchone()) is not None:
+		return fetch[0]
+
+def record(command, *values):
+	cur.execute(command, tuple(values))
+	return cur.fetchone()
+
+def records(command, *values):
+	cur.execute(command, tuple(values))
+	return cur.fetchall()
+
+def column(command, *values):
+	cur.execute(command, tuple(values))
+	return [item[0] for item in cur.fetchall()]
+
+def execute(command, *values):
+	cur.execute(command, tuple(values))
+
+def multiexec(command, valueset):
+	cur.executemany(command, valueset)
+
+def scriptexec(path):
+	with open(path, "r", encoding="utf-8") as script:
+		cur.executescript(script.read())
+
+'''
+
