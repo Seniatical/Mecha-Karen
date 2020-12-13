44a7dd71670059a47dd997d96060a5d78403a477
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-12-13 18:24:34 +0000

    Create emoji.py

---
+++ Helpers/emoji.py
@@ -0,0 +1,24 @@
+import random
+from Helpers import colours
+
+KAREN_FAIL = {
+}
+
+KAREN_SUCESS = {
+}
+
+KARNEN_MIDDLE = {
+}
+
+UNICODE = {
+    "up" : "👍",
+    "down" : "👎",
+    "eyes" : "👀",
+    "comet" : "☄️",
+    "normal_star" : "⭐",
+    "intermediate_star" : "🌟",
+    "shooting_star" : "💫",
+    "good" : "🟢",
+    "alright" : "🟡",
+    "bad" : "🔴"
+}
