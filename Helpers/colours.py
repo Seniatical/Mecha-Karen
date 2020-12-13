ba3b85498aae871562b1bd5b909dd9efe20b4f18
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-12-13 11:21:02 +0000

    Create colours.py

---
+++ Helpers/colours.py
@@ -0,0 +1,56 @@
+BINARY_COLOURS_RGB = {
+    1 : (0, 0, 0)
+    0 : (255, 255, 255)
+}
+
+BINARY_COLOURS_HEX = {
+    1 : 0x000000,
+    0 : 0xFFFFFF
+}
+'''
+Basically these just :
+    BLACK
+    WHITE
+except i represented them in binary form.
+'''
+
+RGB_PRIMARY = {
+    "red" : (255, 0, 0),
+    "yellow" : (0, 255, 0),
+    "blue" : (0, 0, 255)
+}
+
+HEX_PRIMARY = {
+    "red" : 0x0000FF,
+    "yellow" : 0xFF0000,
+    "blue" : 0xFFFF00
+}
+
+RGB_SECONDARY = {
+    "orange" : (255, 102, 0),
+    "green" : (0, 255, 0),
+    "purple" : (102, 0, 255),
+}
+
+RGB_RED_SHADES = {
+    "BASE_COLOUR" : (255, 0, 0),
+    '''
+    All Shades of colour with have "RED" as the main colour
+    Only shades that i use are here.
+    '''
+    "lightsalmon" : (255,160,122),
+    "salmon" : (250,128,114),
+    "darksalmon" : (233,150,122),
+    "lightcoral" : (240,128,128),
+    "indianred" : (205,92,92),
+    "crimson" : (220,20,60),
+    "firebrick" : (178,34,34),
+    "darkred" : (139,0,0),
+    "maroon" : (128,0,0),
+    "tomato" : (255,99,71),
+    "orangered" : (255,69,0),
+    "palevioletred" : (219,112,147),
+}
+
+HEX_RED_SHADES = {
+}
