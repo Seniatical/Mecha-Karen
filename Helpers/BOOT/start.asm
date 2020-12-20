0ac99a42bd270efde2982b36b388c9d8183b0cff
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-12-20 10:57:26 +0000

    Update start.asm

--- Helpers/BOOT/start.asm
+++ Helpers/BOOT/start.asm
@@ -1,29 +1,23 @@
-;------------------------------------------------------------
-.286							   ; CPU type
-;------------------------------------------------------------
-.model TINY						   ; memory of model
-;---------------------- EXTERNS -----------------------------
-extrn				_BootMain:near	   ; prototype of C func
-;------------------------------------------------------------
-;------------------------------------------------------------   
+.286							   
+.model TINY						   
+extrn				_BootMain:near	   
 .code   
 org				07c00h		   ; for BootSector
 main:
 				jmp short start	   ; go to main
 				nop
 						
-;----------------------- CODE SEGMENT -----------------------
 start:	
         cli
-        mov ax,cs               ; Setup segment registers
-        mov ds,ax               ; Make DS correct
-        mov es,ax               ; Make ES correct
-        mov ss,ax               ; Make SS correct        
+        mov ax,cs              
+        mov ds,ax               
+        mov es,ax               
+        mov ss,ax                     
         mov bp,7c00h
-        mov sp,7c00h            ; Setup a stack
+        mov sp,7c00h           
         sti
-                                ; start the program 
+                                
         call           _BootMain
         ret
         
-        END main                ; End of program
+        END main               
