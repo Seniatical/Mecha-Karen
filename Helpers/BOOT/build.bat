dca0d97a414a39b5d31f4ff0e7884f99c1a4529f
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-12-20 10:55:53 +0000

    Update build.bat

--- Helpers/BOOT/build.bat
+++ Helpers/BOOT/build.bat
@@ -3,6 +3,6 @@
 .\VC152\CL.EXE /AT /G2 /Gs /Gx /c /Zl *.cpp
 .\VC152\ML.EXE /AT /c *.asm
 
-.\VC152\LINK.EXE /T /NOD StartPoint.obj bootmain.obj cdisplay.obj cstring.obj
+.\VC152\LINK.EXE /T /NOD StartPoint.obj main.obj cdisplay.obj cstring.obj
 
 del *.obj
