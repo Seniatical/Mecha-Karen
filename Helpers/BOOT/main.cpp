0b981cd91ba48d080fdbc2cc4e45f3656094db80
capslock321 <69331371+capslock321@users.noreply.github.com>
2020-12-20 09:42:14 -0500

    bc i like it that way
    lol

--- Helpers/BOOT/main.cpp
+++ Helpers/BOOT/main.cpp
@@ -2,19 +2,11 @@
 
 #define HELLO_STR               "\"Were Ready To Boot Up! Run the Boot From Utils to complete.\"10s Till Automatic Shutdown"
 
-extern "C" void BootMain()
-{
+extern "C" void BootMain(){
     CDisplay::ClearScreen();
     CDisplay::ShowCursor(false);
 
-    CDisplay::TextOut(
-        HELLO_STR,
-        0,
-        0,
-        BLACK,
-        WHITE,
-        false
-        );
+    CDisplay::TextOut(HELLO_STR,0,0,BLACK,WHITE,false);
 
-    return;
+    return true;
 }
