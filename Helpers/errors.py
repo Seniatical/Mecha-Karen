cd03d2c9d314ac0bf6ef366e1b44c2dda4eb0e01
Seniatical <69202104+Seniatical@users.noreply.github.com>
2020-12-20 18:33:28 +0000

    Create errors.py

---
+++ Helpers/errors.py
@@ -0,0 +1,17 @@
+class BASE(Exception):
+    pass
+
+class NotGuildOwner(BASE):
+    pass
+    
+class NotInVoice(BASE):
+    pass
+    
+class NotUpdates(BASE):
+    pass
+    
+class NotBot(BASE):
+    pass
+    
+class ThreadingError(BASE):
+    pass
