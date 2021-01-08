3cea00cef8a00ecfe94df6aff378dfa255939401
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-01-08 13:02:50 +0000

    moved from ..Mecha Karen

---
+++ Bot/__main__.py
@@ -0,0 +1,43 @@
+# !/usr/bin/python
+
+import os
+import shutin
+from Utils import __logging__
+from Utils import setup
+from Utils.sensitive import env_reader
+import bot
+
+class Error(Exception):
+    pass
+    
+if not __name__ == '__main__':
+    raise Error('Not __main__.py file being ran!')
+  
+current_dir = os.getcwd()
+
+if not current_dir.endswith('Bot'):
+    raise Error('Move __main__.py into the Bot folder')
+    
+data = env_reader(get='*')
+    
+start = __logging__.BOOT(
+    username = data['USERNAME']
+    password = data['PASSWORD']
+    host = 'https://127.0.0.1:9785',
+    is_local = True
+    server = data['SERVER_INFO']['ADDRESS']
+    raise_on_warnings = True
+    return_traceback = True
+    lint = '*'
+    time = __logging__.last_triggered_at
+    )
+    
+if not start:
+    raise Error('Failed to start up __logging__\n\n%s' % __logging__.dumps('ERROR')[0])
+    
+start.start(
+    setup.setup()
+)
+
+self = bot.Mecha_Karen()
+self.run()
