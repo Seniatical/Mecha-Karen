92b07642b7e1ae95d12760ca6ad931655fead49f
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-01-17 18:12:55 +0000

    fixed missing param

--- Bot/Helpers/queries.py
+++ Bot/Helpers/queries.py
@@ -1,15 +1,26 @@
 user_data = '''
-/*
-Selects all the data we need from the database
-
-Its stores guild to guild
-
-Makes warning systems easier to manage
-*/
 SELECT 
     (Strikes, Logged_In, Registered_At, Messages, Bot, Commands, Email, Password, Servers, Premium, LastUpdate, Admin, SupportBoosts)
 FROM 
     "users" 
 WHERE 
     UserID = ? AND GuildID = ? AND name LIKE %GuildID%
+'''
+
+warns = '''
+SELECT
+    *
+FROM
+    "warns"
+WHERE
+    UserID = ? AND GuildID = ? AND name LIKE %warn%
+'''
+
+starboard = '''
+SELECT
+    *
+FROM
+    "starboard"
+WHERE
+    GuildID = ?
 '''
