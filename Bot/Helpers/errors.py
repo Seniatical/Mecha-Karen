431764ef8b19c90694fcd1598eca371d2ba095cc
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-01-25 10:18:20 +0000

    Update errors.py

--- Bot/Helpers/errors.py
+++ Bot/Helpers/errors.py
@@ -1,3 +1,19 @@
+# !/usr/bin/python
+
+"""
+Copyright ©️: 2020 Seniatical / _-*™#7519
+License: Apache 2.0
+
+A permissive license whose main conditions require preservation of copyright and license notices.
+Contributors provide an express grant of patent rights.
+Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+FULL LISENCE CAN BE FOUND:
+    https://www.apache.org/licenses/LICENSE-2.0.html
+
+Any voilations to the lisence, will result in moderate action
+"""
+
 class BASE(Exception):
     pass
 
@@ -14,4 +30,10 @@ class NotBot(BASE):
     pass
     
 class ThreadingError(BASE):
+    pass
+
+class ConnectionError(BASE):
+    pass
+
+class BaseRAW(BASE):
     pass
