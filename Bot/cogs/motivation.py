e4ace9fe732afcfc8f9285aeff3719fccae2a86d
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-01-25 10:32:20 +0000

    Removed Art & License Updates

--- Bot/cogs/motivation.py
+++ Bot/cogs/motivation.py
@@ -1,3 +1,21 @@
+# !/usr/bin/python
+
+"""
+Copyright ©️: 2020 Seniatical / _-*™#7519
+License: Apache 2.0
+
+A permissive license whose main conditions require preservation of copyright and license notices.
+Contributors provide an express grant of patent rights.
+Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+FULL LISENCE CAN BE FOUND:
+    https://www.apache.org/licenses/LICENSE-2.0.html
+
+Any voilations to the lisence, will result in moderate action
+
+Your required to mention (original author, lisence, source, any changes made)
+"""
+
 import discord
 from discord.ext import commands
 from datetime import timedelta
@@ -56,16 +74,6 @@ class motivation(commands.Cog):
             '**Your stronger than you think. Dont keep your dream in your mind. Show it. Prove the world what your made of.**‏‏'))
         await ctx.send(embed=embed)
         await ctx.send(file=File(f'{random.choice(IMG.speech)}'))
-
-    @commands.command(aliases=['masterpiece', 'Arts', 'mp', 'Masterpieces'])
-    @cooldown(1, 180, BucketType.user)
-    async def Art(self, ctx):
-        embed = discord.Embed(
-            title='**Artworks:**',
-            color=discord.Color.dark_blue()
-        )
-        embed.add_field(name='‏‏‎ ‎', value=f'{random.choice(art.art)}')
-        await ctx.send(embed=embed)
 
 def setup(bot):
     bot.add_cog(motivation(bot))
