139adaded3c58172d436753c2d8689d2a64e6957
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-01-25 10:15:47 +0000

    License Updates

--- Bot/__main__.py
+++ Bot/__main__.py
@@ -1,5 +1,19 @@
 # !/usr/bin/python
 
+"""
+Copyright ©️: 2020 Seniatical / _-*™#7519
+License: Apache 2.0
+
+A permissive license whose main conditions require preservation of copyright and license notices.
+Contributors provide an express grant of patent rights.
+Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+FULL LISENCE CAN BE FOUND:
+    https://www.apache.org/licenses/LICENSE-2.0.html
+
+Any voilations to the lisence, will result in moderate action
+"""
+
 import os
 import shutil
 from Utils import __logging__
