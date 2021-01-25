88b81ac844b1ea2ca90d92b75b2486fb24033a1a
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-01-25 10:21:53 +0000

    New statements & License updates

--- Bot/Helpers/queries.py
+++ Bot/Helpers/queries.py
@@ -1,3 +1,17 @@
+"""
+Copyright ©️: 2020 Seniatical / _-*™#7519
+License: Apache 2.0
+
+A permissive license whose main conditions require preservation of copyright and license notices.
+Contributors provide an express grant of patent rights.
+Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+
+FULL LISENCE CAN BE FOUND:
+    https://www.apache.org/licenses/LICENSE-2.0.html
+
+Any voilations to the lisence, will result in moderate action
+"""
+
 user_data = '''
 SELECT 
     (Strikes, Logged_In, Registered_At, Messages, Bot, Commands, Email, Password, Servers, Premium, LastUpdate, Admin, SupportBoosts)
@@ -13,7 +27,7 @@ SELECT
 FROM
     "warns"
 WHERE
-    UserID = ? AND GuildID = ? AND name LIKE %warn%
+    UserID = ? AND GuildID = ? AND name LIKE %warn% AND CASE = ?
 '''
 
 starboard = '''
@@ -23,4 +37,11 @@ FROM
     "starboard"
 WHERE
     GuildID = ?
+'''
+
+add_to_db = '''
+INSERT INTO Users
+    (Strikes, Logged_In, Registered_At, Messages, Bot, Commands, Email, Password, Servers, Premium, LastUpdate, Admin, SupportBoosts)
+VALUES
+    (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)
 '''
