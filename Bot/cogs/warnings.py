11da4e1b08d75cc72ce3a0e164a95f6eac05e306
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-01-30 12:13:32 +0000

    DB Rollover

--- Bot/cogs/warnings.py
+++ Bot/cogs/warnings.py
@@ -18,137 +18,90 @@ You are legally required to mention (original author, license, source and any ch
 
 import discord
 from discord.ext import commands
-from Utils import db
-import json
 import datetime
 
 class warnings(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+        self.client = bot.client
 
     @commands.command()
     @commands.has_guild_permissions(manage_messages=True)
     @commands.cooldown(1, 15, commands.BucketType.user)
-    async def warn(self, ctx, user: discord.Member = None, *, reason: str = None):
-        if not user:
-            return await ctx.send(embed=discord.Embed(
-                description='<a:nope:787764352387776523> Need to provide a user to warn!',
-                colour=discord.Colour.red()
-            ), mention_author=False)
-        if user.top_role > ctx.author.top_role:
+    async def warn(self, ctx, user: discord.Member, *, reason: str = 'Not provided.'):
+        if user.top_role > ctx.author.top_role and not ctx.author == ctx.guild.owner:
             return await ctx.message.reply(embed=discord.Embed(
                 description='<a:nope:787764352387776523> Cannot warn members with a higher role then you!',
                 colour=discord.Colour.red()
             ), mention_author=False)
+        if user.bot:
+            return await ctx.message.reply(embed=discord.Embed(
+                description='<a:nope:787764352387776523> Cannot warn bots!',
+                colour=discord.Colour.red()
+            ), mention_author=False)     
         if user == ctx.author:
             return await ctx.message.reply(embed=discord.Embed(
                 description='<a:nope:787764352387776523> Cannot warn yourself!',
                 colour=discord.Colour.red()
             ), mention_author=False)
-        if not reason:
-            return await ctx.message.reply(embed=discord.Embed(
-                description='<a:nope:787764352387776523> Need to provide a reason for the warn!',
-                colour=discord.Colour.red()
-            ), mention_author=False)
 
-        action = db.record(
-            'SELECT ModAction FROM guild WHERE GuildID = ?', ctx.guild.id) or (0,)
-        current_warns = db.record(
-            'SELECT warnings FROM guild WHERE GuildID = ?', ctx.guild.id) or (None,)
-        if not current_warns[0]:
-            former = {
-                user.id: {
-                    1: {
-                        'moderator': str(ctx.author),
-                        'reason': reason,
-                        'warned_at': datetime.datetime.utcnow().strftime('%d/%m/%Y - %I:%M %p'),
-                        'num': action[0]+1
-                    }
-                }
-            }
-            stringed = json.dumps(former)
-            db.execute(
-                'UPDATE guild SET warnings = ? WHERE GuildID = ?', stringed, ctx.guild.id)
-            await ctx.message.reply(embed=discord.Embed(
-                description='<a:Passed:757652583392215201> **Successfully** logged warning for {}'.format(user.mention),
-                colour=discord.Colour.green()
-            ))
-        else:
-            data = current_warns[0]
-            dict_ = json.loads(data)
-            if str(user.id) not in dict_:
-                dict_[str(user.id)] = {
-                    1: {
-                        'moderator': str(ctx.author),
-                        'reason': reason,
-                        'warned_at': datetime.datetime.utcnow().strftime('%d/%m/%Y - %I:%M %p'),
-                        'num': action[0]+1
-                    }
-                }
-            else:
-                dict_[str(user.id)][str(len(dict_[str(user.id)])+1)] = {
-                    'moderator': str(ctx.author),
-                    'reason': reason,
-                    'warned_at': datetime.datetime.utcnow().strftime('%d/%m/%Y - %I:%M %p'),
-                    'num': action[0]+1
-                    }
-            stringed = json.dumps(dict_)
-            db.execute(
-                'UPDATE guild SET warnings = ? WHERE GuildID = ?', stringed, ctx.guild.id)
-            db.execute(
-                'UPDATE guild SET ModAction = ? WHERE GuildID = ?', action[0]+1, ctx.guild.id)
-            await ctx.message.reply(embed=discord.Embed(
-                description='<a:Passed:757652583392215201> **Successfully** warned **{}**'.format(user),
-                colour=discord.Colour.green()
-            ))
-        db.commit()
+        table = self.client['Bot']
+        column = table['Warns']
+
+        warns = column.find_one({'_id': f'{ctx.guild.id}/{user.id}'})
+        if not warns:
+            column.insert_one({'_id': f'{ctx.guild.id}/{user.id}', 'warnings': []})
+        warns = column.find_one({'_id': f'{ctx.guild.id}/{user.id}'})
+
+        warns['warnings'].append({
+                                'warned_at': datetime.datetime.utcnow().strftime('%d/%m/%Y - %I:%M %p'),
+                                'moderator': f'{ctx.author} ({ctx.author.id})',
+                                'reason': reason
+        })
+        column.update_one({'_id':  f'{ctx.guild.id}/{user.id}'}, {'$set': {'warnings': warns['warnings']}})
+
+        await ctx.message.reply(embed=discord.Embed(
+            description='<a:Passed:757652583392215201> Successfully logged warning for {}'.format(user),
+            colour=discord.Colour.red()
+            ), mention_author=False)
 
     @commands.command(aliases=['warns'])
     @commands.has_guild_permissions(manage_messages=True)
     @commands.cooldown(1, 15, commands.BucketType.user)
-    async def warnings(self, ctx, user: discord.Member = None):
-        user = user or ctx.author
-        warnings_ = db.record(
-            'SELECT warnings FROM guild WHERE GuildID = ?', ctx.guild.id) or (None,)
-        if not warnings_[0]:
-            await ctx.send(embed=discord.Embed(
-                description='**The user {} has no warnings.**'.format(user),
+    async def warnings(self, ctx, user: discord.Member):
+        table = self.client['Bot']
+        column = table['Warns']
+
+        warns = column.find_one({'_id': f'{ctx.guild.id}/{user.id}'})
+
+        if not warns:
+            return await ctx.send(embed=discord.Embed(
+                description="<a:nope:787764352387776523> The user **{}** doesn't have any warns".format(user),
                 colour=discord.Colour.red()
-            ))
-        else:
-            dict_ = json.loads(warnings_[0])
-            try:
-                user_ = dict_[str(user.id)]
-            except KeyError:
-                return await ctx.send(embed=discord.Embed(
-                    description="<a:nope:787764352387776523> The user **{}** doesn't have any warns".format(user),
-                    colour=discord.Colour.red()
-                ))
-            warns = len(dict_[str(user.id)])
-            embed = discord.Embed(
-                colour=discord.Colour.red(),
-                title='📖 Logs:'.format(warns),
-                timestamp=datetime.datetime.utcnow()).set_footer(
-                text='Prompted by {}'.format(ctx.author), icon_url=ctx.author.avatar_url)
-            embed.set_author(icon_url=user.avatar_url, name=user)
-            for i in range(len(user_)):
-                reason = dict_[str(user.id)][str(i+1)]['reason']
-                mod = dict_[str(user.id)][str(i+1)]['moderator']
-                time = dict_[str(user.id)][str(i+1)]['warned_at']
-                embed.add_field(
-                    name='#{} | Warn | {}'.format(str(i+1), time),
-                    value='Responsible Mod: {}\nReason: {}'.format(mod, reason), inline=False)
-            await ctx.send(embed=embed)
+            ), mention_author=False)
+        
+        warns_ = len(warns['warnings'])
+        
+        embed = discord.Embed(
+            colour=discord.Colour.red(),
+            title='📖 Logs:'.format(warns),
+            timestamp=datetime.datetime.utcnow()).set_footer(
+            text='Prompted by {}'.format(ctx.author), icon_url=ctx.author.avatar_url)
+        embed.set_author(icon_url=user.avatar_url, name=user)
+        
+        for i in range(warns_):
+            reason = warns['warnings'][i]['reason']
+            mod = warns['warnings'][i]['moderator']
+            time = warns['warnings'][i]['warned_at']
+            embed.add_field(
+                name='#{} | Warn | {}'.format(str(i+1), time),
+                value='Responsible Mod: {}\nReason: {}'.format(mod, reason), inline=False)
+        await ctx.message.reply(embed=embed, mention_author=False)
 
     @commands.command()
     @commands.has_guild_permissions(manage_messages=True)
     @commands.cooldown(1, 15, commands.BucketType.user)
-    async def delwarn(self, ctx, user: discord.Member = None, warn: str = '0'):
-        if warn == '0':
-            return await ctx.send(embed=discord.Embed(
-                description='<a:nope:787764352387776523> Need to provide a warn number!',
-                colour=discord.Colour.red()
-            ))
+    async def delwarn(self, ctx, user: discord.Member, warn: str):
         try:
             warn = int(warn)
         except ValueError:
@@ -156,40 +109,31 @@ class warnings(commands.Cog):
                 description='<a:nope:787764352387776523> Number provided must be a number not letters!',
                 colour=discord.Colour.red()
             ))
-        if not user:
-            return await ctx.send(embed=discord.Embed(
-                description='<a:nope:787764352387776523> Provide a member!',
-                colour=discord.Colour.red()
-            ))
-        warns = db.record(
-            'SELECT warnings FROM guild WHERE GuildID = ?', ctx.guild.id) or (None,)
-        if not warns[0]:
+
+        table = self.client['Bot']
+        column = table['Warns']
+        warns = column.find_one({'_id': f'{ctx.guild.id}/{user.id}'})
+
+        if not warns:
             return await ctx.send(embed=discord.Embed(
                 description='<a:nope:787764352387776523> User doesnt have any warns!',
                 colour=discord.Colour.red()
             ))
-        warns = json.loads(warns[0])
-        if len(warns[str(user.id)]) < warn or warn < 1:
+        try:
+            warns['warnings'].pop(warn-1)
+        except IndexError:
             return await ctx.send(embed=discord.Embed(
-                description='<a:nope:787764352387776523> Warn number was not found!',
+                description='<a:nope:787764352387776523> Warn number not found!',
                 colour=discord.Colour.red()
             ))
-        if user == ctx.author:
-            return await ctx.send(embed=discord.Embed(
-                description='<a:nope:787764352387776523> Cannot remove your own warns!'
-            ))
-        del warns[str(user.id)][str(warn)]
-        if len(warns[str(user.id)]) == 0:
-            del warns[str(user.id)]
-        stringed = json.dumps(warns)
-        db.execute(
-            'UPDATE guild SET warnings = ? WHERE GuildID = ?', stringed, ctx.guild.id)
-        db.commit()
+        if len(warns['warnings']) == 0:
+            column.delete_one({'_id': f'{ctx.guild.id}/{user.id}'})
+        else:
+            column.update_one({'_id': f'{ctx.guild.id}/{user.id}'}, {'$set': {'warnings': warns['warnings']}})
         await ctx.send(embed=discord.Embed(
-            description='<a:Passed:757652583392215201> Deleted warning number **{}** for user **{}**.'.format(warn, user),
-            colour=discord.Colour.green()
-        ))
-
-
+            colour=discord.Colour.green(),
+            description='<a:Passed:757652583392215201> Removed warn #{} for user **{}**'.format(warn, user)
+            ))
+        
 def setup(bot):
     bot.add_cog(warnings(bot))
