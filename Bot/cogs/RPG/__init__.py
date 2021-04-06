dd67892d27de3a02ecb5f798e56c716799785aa8
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-04-06 14:50:24 +0100

    Update __init__.py

--- Bot/cogs/RPG/__init__.py
+++ Bot/cogs/RPG/__init__.py
@@ -1,1 +1,4 @@
-from .cog import Cog
+from .cog import Cog  ## MAIN COG FOR THE RPG
+from .support import Support  ## HELP COMMANDS / TUTORIALS
+from .use import Use ## Commands to use special items and stuff
+from .owner import EcoOwner ## Owner commands
