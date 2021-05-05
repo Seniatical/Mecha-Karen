9f539c737a9361d978f25a8fa3f2770fbc6b8b9c
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-05-05 19:48:09 +0100

    Shoved scrapers back into here

--- Bot/cogs/fun.py
+++ Bot/cogs/fun.py
@@ -642,6 +642,93 @@ class fun(commands.Cog):
         except Exception:
             return await ctx.message.reply(content='I have no response for you, now go away.')
         return await ctx.message.reply(content=real['response']['answer'] or 'Hello There.')
+                        
+    @commands.command()
+    @commands.cooldown(1, 10, commands.BucketType.user)
+    async def urban(self, ctx, *, word : str=None):
+        message = await ctx.send('Fetching Your Word From `Urban Dictionary`!')
+        data = UD.search(word)
+        try:
+            name = 'User Not Found' if data.author['name'] == "" else data.author['name']
+            embed = discord.Embed(
+                title='Search Results for **{}**'.format(data.the_word.title()),
+                description='**Contributed by {}**'.format(name),
+                colour=discord.Color.dark_blue(),
+                timestamp=datetime.datetime.utcnow()
+            ).set_footer(text='Prompted by {}'.format(ctx.author), icon_url=ctx.author.avatar_url)
+            embed.add_field(name='Meaning:', value=data.meaning)
+            embed.add_field(name='Example:', value='N/A' if data.example == "" else data.example, inline=False)
+            embed.add_field(name='Likes:', value='👍 {}'.format(data.rating['likes']))
+            embed.add_field(name='Dislikes:', value='👎 {}'.format(data.rating['dislikes']))
+            year, month, day = int(data.date['year']), int(data.date['date'].split('-')[1]), int(data.date['day'])
+            embed.add_field(name='Posted At:', value=datetime.date(year, month, day).strftime('%A, %B %Y') + ' ({})'.format(data.date['date']), inline=False)
+            await message.edit(content=None, embed=embed)
+        except KeyError:
+            await message.edit('{} The word **`{}`** wasnt found!'.format(ctx.author.mention, word.title()))
+
+    @commands.command()
+    @commands.bot_has_guild_permissions(kick_members=True, create_instant_invite=True)
+    async def vanish(self, ctx):
+        global xyz
+
+        async def message():
+            try:
+                invites = await ctx.guild.invites()
+                y = await ctx.author.send('You have vanished.\nRejoin: {}'.format(random.choice(invites)))
+                return y
+            except Exception:
+                return
+        try:
+            check:discord.Message = await ctx.send('Are your sure that you want to vanish?')
+            await check.add_reaction('✅')
+            await check.add_reaction('❌')
+            xyz = False
+
+            def _check(m):
+                global xyz
+                if m.member.id == ctx.author.id and str(m.emoji) == '✅':
+                    xyz = True
+                    return True
+                elif m.member.id == ctx.author.id and str(m.emoji) == '❌':
+                    return True
+                return False
+            await self.bot.wait_for('raw_reaction_add', check=_check)
+            if not xyz:
+                await check.clear_reactions()
+                return await check.edit(content='Looks like **{}** doesnt want to vanish!'.format(ctx.author))
+            await ctx.guild.kick(ctx.author, reason='They have VANISHED.')
+            try:
+                await message()
+            except discord.errors.Forbidden:
+                return
+            await check.edit(content='**{}** Has Vanished.'.format(ctx.author))
+            await check.clear_reactions()
+            await check.add_reaction(emoji.KAREN_ADDITIONS_ANIMATED['wave'])
+        except discord.errors.Forbidden:
+            await check.edit(content="Failed to secretly move {}'s Fat Ass.".format(ctx.author.mention))
+            await check.clear_reactions()
+            return await check.add_reaction('<:F_:745287381816574125>')
+
+    @commands.command()
+    @commands.cooldown(1, 30, commands.BucketType.user)
+    async def image(self, ctx, query:str = None):
+        if not query:
+            return await ctx.send('Need to give an image to search for!')
+        url = 'https://api.pexels.com/v1/search?query={}&per_page={}'.format(query, random.randint(1, 100))
+        auth = 'API KEY_HERE'
+        ## Returns stock images so it wont find everything and wont find NSFW as well
+        r = requests.get(url, headers={'Authorization' : auth}).json()
+        try:
+            await ctx.send(
+                embed=discord.Embed(
+                    title='Search results for {}'.format(
+                        query.title()
+                    ),
+                    colour=discord.Color.red(),
+                ).set_image(url=random.choice(r['photos'])['src']['large2x'])
+            )
+        except IndexError:
+            return await ctx.send('No Image was Found Under the Context **{}**'.format(query.title()))
     
 def setup(bot):
     bot.add_cog(fun(bot))
