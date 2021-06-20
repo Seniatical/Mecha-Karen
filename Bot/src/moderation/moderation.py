0a8275e70de6ea7b6891b544ffc57c8c2f7ba74c
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-06-20 16:39:47 +0100

    CC Notice

--- Bot/src/moderation/moderation.py
+++ Bot/src/moderation/moderation.py
@@ -1,3 +1,17 @@
+# !/usr/bin/python
+
+"""
+Copyright ©️: 2020 Seniatical / _-*™#7519
+License: Apache 2.0
+A permissive license whose main conditions require preservation of copyright and license notices.
+Contributors provide an express grant of patent rights.
+Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+FULL LICENSE CAN BE FOUND AT:
+    https://www.apache.org/licenses/LICENSE-2.0.html
+Any violation to the license, will result in moderate action
+You are legally required to mention (original author, license, source and any changes made)
+"""
+
 import typing
 
 import discord
@@ -27,7 +41,7 @@ def convert(time: int, unit):
         return ((time * 60) * 60) * 24
 
 
-class moderation(commands.Cog):
+class Moderation(commands.Cog):
 
     def __init__(self, bot):
         self.bot = bot
