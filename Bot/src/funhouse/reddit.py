91164178f390dc1ce2aae500af42ce4bddca031e
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-06-20 13:03:41 +0100

    CC Notice

--- Bot/src/funhouse/reddit.py
+++ Bot/src/funhouse/reddit.py
@@ -1,3 +1,17 @@
+# !/usr/bin/python
+
+"""
+Copyright ©️: 2020 Seniatical / _-*™#7519
+License: Apache 2.0
+A permissive license whose main conditions require preservation of copyright and license notices.
+Contributors provide an express grant of patent rights.
+Licensed works, modifications, and larger works may be distributed under different terms and without source code.
+FULL LICENSE CAN BE FOUND AT:
+    https://www.apache.org/licenses/LICENSE-2.0.html
+Any violation to the license, will result in moderate action
+You are legally required to mention (original author, license, source and any changes made)
+"""
+
 import random
 import datetime
 import time
@@ -20,42 +34,12 @@ class Reddit(commands.Cog):
         self.bot = bot
         self.all_subs = []
         self.reddit = apraw.Reddit(
-            username='Seniatical',
-            password='ilikemrbean',
-            client_id='cwmSyyiijyG0Wg',
-            client_secret='RbujK11eDKYIiLeAf3zzGyQGcFg',
-            user_agent='Mecha Karen Memes Refresh'
+            username=bot.env('REDDIT_USERNAME'),
+            password=bot.env('REDDIT_PASSWORD'),
+            client_id=bot.env('REDDIT_CLIENT_ID'),
+            client_secret=bot.env('REDDIT_CLIENT_SECRET'),
+            user_agent='<Mecha Karen - Discord Bot [https://github.com/Seniatical/Mecha-Karen/main/Bot/src/funhouse/reddit.py]>'
         )
-    @commands.Cog.listener()
-    async def on_ready(self):
-        async def speed():
-            while True:
-                sub = await self.reddit.subreddit('memes')
-                async for _submission in sub.top(limit=500):
-                    if not _submission:
-                        continue
-
-                    name = _submission.title
-                    url = _submission.url
-                    comments = _submission.num_comments
-                    up = _submission.score
-                    sub = await _submission.subreddit()
-                    name_ = await _submission.author()
-
-                    embed = discord.Embed(
-                        title=name,
-                        color=discord.colour.Color.from_rgb(random.randint(1, 255), random.randint(1, 255), random.randint(1, 255)),
-                        url = await self.bot.loop.run_in_executor(None, requote_uri, f'https://www.reddit.com/r/dankmemes/comments/{_submission.id}/{_submission.title}/')
-                    )
-                    try:
-                        embed.set_author(name='Posted by {} from r/{}'.format(name_, sub), icon_url=name_.icon_img, url=f'https://www.reddit.com/user/{name_}/')
-                    except Exception:
-                        embed.set_author(name='Posted by {} from r/{}'.format(name_, sub), url=f'https://www.reddit.com/user/{name_}/')
-                    embed.set_image(url=url)
-                    embed.set_footer(text='{} {}   |   {} {}'.format(emoji.UNICODE['up'], convert_size(up), emoji.UNICODE['text'], convert_size(comments)))
-                    self.all_subs.append(embed)
-                await asyncio.sleep(3600)
-        self.bot.loop.create_task(speed())
 
     @commands.command(name='Meme', aliases=['memes'])
     @commands.cooldown(1, 5, BucketType.user)
