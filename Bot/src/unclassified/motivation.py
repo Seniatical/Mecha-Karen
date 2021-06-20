2e362d585e84aeedde93e8199f76000be2633d3a
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-06-20 12:44:44 +0100

    Unclassified commands / cogs section

---
+++ Bot/src/unclassified/motivation.py
@@ -0,0 +1,46 @@
+import discord
+from discord.ext import commands
+from datetime import timedelta
+from discord.ext.commands import BucketType, cooldown
+from discord import File
+import random
+import os
+
+from utility.quotes import words, images
+
+class Motivation(commands.Cog):
+
+    def __init__(self, bot):
+        self.bot = bot
+        self.speech_paths = []
+
+        for file in os.listdir('./storage/speeches'):
+            if os.path.isdir(file):
+                for _file in os.listdir(f'./storage/speeches/{file}'):
+                    self.speech_paths.append(file + _file)
+            else:
+                self.speech_paths.append('./speeches/' + file)
+
+    @commands.command(aliases=['Quotes'])
+    @cooldown(1, 10, BucketType.user)
+    async def quote(self, ctx):
+        return await ctx.send(embed=discord.Embed(
+            description=random.choice(words),
+            colour=discord.Colour.gold()
+        ))
+
+    @commands.command(aliases=['VQ', 'ImgQ', 'IQuote'])
+    @cooldown(1, 15, BucketType.user)
+    async def imagequote(self, ctx):
+        return await ctx.send(embed=discord.Embed(
+            colour=discord.Colour.gold(),
+        ).set_image(url=random.choice(images)))
+
+    @commands.command(aliases=['Speeches'])
+    @cooldown(1, 120, BucketType.user)
+    async def speech(self, ctx):
+        return await ctx.send(content='Enjoy this speech to listen to!',
+                              file=discord.File(random.choice(self.speech_paths), filename='speech.mp3'))
+
+def setup(bot):
+    bot.add_cog(Motivation(bot))
