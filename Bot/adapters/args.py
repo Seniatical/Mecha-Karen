b053d5421c777c29d085782d2bdc51b16ef3e8dc
Seniatical <69202104+Seniatical@users.noreply.github.com>
2021-06-26 16:32:10 +0100

    Add argument adapter

---
+++ Bot/adapters/args.py
@@ -0,0 +1,41 @@
+from TagScriptEngine import Adapter, Verb, escape_content
+
+
+class ArgumentAdapter(Adapter):
+    r""" A class which will parse `*args` into your tag """
+
+    def __init__(self, *args):
+        self.args = args
+        self._methods = {}
+
+        self.object = self.__repr__()
+        super().__init__()
+
+    def __repr__(self):
+        return "< TagArguments [{!r}] >".format(len(self.args))
+
+    def get_value(self, ctx: Verb):
+        should_escape = False
+
+        if not ctx.parameter:
+            return str(self.object)
+
+        try:
+            value = self.args[int(ctx.parameter)]
+        except (IndexError, ValueError):
+            if method := self._methods.get(ctx.parameter):
+                value = method()
+            else:
+                return str()
+
+        if isinstance(value, tuple):
+            value, should_escape = value
+
+        if not value:
+            return_value = str()
+        else:
+            return_value = str(value)
+
+        if should_escape:
+            return escape_content(return_value)
+        return return_value
